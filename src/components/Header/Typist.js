c6d4d86ce18316fdd9df4249259382c2e348e804
alexluong <aluong@crimson.ua.edu>
2018-05-20 14:50:56 -0500

    Align Typist

--- src/components/Header/Typist.js
+++ src/components/Header/Typist.js
@@ -2,11 +2,12 @@ import styled      from 'styled-components';
 
 const TypistWrapper = styled.div`
   display: ${props => props.isHome ? 'block' : 'none'};
+  width: 100%;
+  text-align: center;
   color: #fff;
   position: absolute;
-  left: 50%;
   top: 50%;
-  transform: translate(-50%, -50%);
+  transform: translateY(-50%);
   * {
     font-size: 4.6rem;
   }
