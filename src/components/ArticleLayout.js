db353543ce8fb6fb82e4c316cce47cdb6b690922
Alex Luong <aluong@crimson.ua.edu>
2018-11-29 04:38:25 -0600

    feat: Layout

--- src/components/ArticleLayout.js
+++ src/components/ArticleLayout.js
@@ -4,34 +4,20 @@ const Article = styled.div`
   div:nth-child(2) {
     display: grid;
     grid-template-columns:
-      minmax(2.6rem, 1fr) minmax(0, 15rem) minmax(auto, 65rem)
-      minmax(0, 15rem) minmax(2.6rem, 1fr);
+      minmax(2.6rem, 1fr)
+      minmax(auto, 70ch)
+      minmax(2.6rem, 1fr);
 
     & > * {
-      grid-column: 3;
+      grid-column: 2;
     }
 
     .gatsby-image-wrapper,
     .gatsby-highlight {
-      grid-column: 2 / 5;
-      justify-self: stretch;
-      margin: 3rem 0;
-    }
-
-    @media only screen and (max-width: 1060px) {
-      grid-template-columns: minmax(2.6rem, 1fr) minmax(auto, 70ch) minmax(
-          2.6rem,
-          1fr
-        );
-
-      & > * {
-        grid-column: 2;
-      }
-
-      .gatsby-image-wrapper,
-      .gatsby-highlight {
-        grid-column: 1 / 4;
-      }
+      grid-column: 1 / 4;
+      margin: 3rem auto;
+      max-width: 100ch;
+      width: 100%;
     }
   }
 `
