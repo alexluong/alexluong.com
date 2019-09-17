77a0373d1e6025d2659fbdda3c59abde48734db2
Alex Luong <aluong@crimson.ua.edu>
2019-09-17 05:06:28 -0500

    feat: set up Article interface

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,36 @@ module.exports = {
     "gatsby-transformer-sharp",
 
     {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1380,
+              linkImagesToOriginal: false,
+            },
+          },
+          { resolve: "gatsby-remark-copy-linked-files" },
+          { resolve: "gatsby-remark-smartypants" },
+        ],
+        remarkPlugins: [require("remark-slug")],
+      },
+    },
+
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
         path: `${__dirname}/content/assets`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "articles",
+        path: `${__dirname}/content/articles`,
       },
     },
   ],
---
+++ gatsby-node.js
@@ -0,0 +1,83 @@
+const crypto = require("crypto")
+
+const mdxResolverPassthrough = fieldName => async (
+  source,
+  args,
+  context,
+  info,
+) => {
+  const type = info.schema.getType(`Mdx`)
+  const mdxNode = context.nodeModel.getNodeById({
+    id: source.parent,
+  })
+  const resolver = type.getFields()[fieldName].resolve
+  const result = await resolver(mdxNode, args, context, {
+    fieldName,
+  })
+  return result
+}
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions
+
+  createTypes(`interface Article @nodeInterface {
+    id: ID!
+    title: String!
+    body: String!
+    slug: String!
+    date: Date! @dateformat
+  }`)
+
+  createTypes(
+    schema.buildObjectType({
+      name: "MdxArticle",
+      fields: {
+        id: { type: "ID!" },
+        title: { type: "String!" },
+        slug: { type: "String!" },
+        date: { type: "Date!", extensions: { dateformat: {} } },
+        body: {
+          type: "String!",
+          resolve: mdxResolverPassthrough("body"),
+        },
+      },
+      interfaces: ["Node", "Article"],
+    }),
+  )
+}
+
+exports.onCreateNode = async ({ node, actions, getNode, createNodeId }) => {
+  const { createNode, createParentChildLink } = actions
+
+  if (node.internal.type !== "Mdx") {
+    return
+  }
+
+  const fileNode = getNode(node.parent)
+
+  if (fileNode.sourceInstanceName === "articles") {
+    const fieldData = {
+      slug: node.frontmatter.slug || fileNode.relativeDirectory,
+      date: node.frontmatter.date,
+      title: node.frontmatter.title,
+    }
+
+    const mdxArticleId = createNodeId(`${node.id} >>> MdxArticle`)
+    await createNode({
+      ...fieldData,
+      id: mdxArticleId,
+      parent: node.id,
+      children: [],
+      internal: {
+        type: "MdxArticle",
+        contentDigest: crypto
+          .createHash("md5")
+          .update(JSON.stringify(fieldData))
+          .digest("hex"),
+        content: JSON.stringify(fieldData),
+        description: "Mdx implementation of the Article interface",
+      },
+    })
+    createParentChildLink({ parent: node, child: getNode(mdxArticleId) })
+  }
+}
