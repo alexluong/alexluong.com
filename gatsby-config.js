5b0bdcc74b1833713085759174a39cdf5afca410
Alex Luong <aluong@crimson.ua.edu>
2019-09-17 04:14:38 -0500

    feat: reset to blank Gatsby site for v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,117 +1,14 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
-  siteMetadata: {
-    title: "Alex Luong",
-    description: "Hi. I'm Alex, and I write about web development.",
-    canonicalUrl: "https://blog.alexluong.com",
-    social: {
-      fbAppId: "",
-      twitter: "@alexluongg_",
-    },
-
-    // for schema.org
-    organization: {
-      name: "Alex Luong",
-      url: "https://www.alexluong.com",
-      logo: "",
-    },
-
-    // prefix
-    prefix: {
-      post: "blog",
-      category: "tag",
-    },
-  },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
-    "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    "gatsby-plugin-eslint",
-    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
 
     {
-      resolve: "gatsby-source-contentful",
+      resolve: "gatsby-source-filesystem",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
-
-    // Transform markdown files to GraphQL nodes
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 590,
-            },
-          },
-          "gatsby-remark-smartypants",
-          "gatsby-remark-emojis",
-          "gatsby-remark-prismjs",
-        ],
-      },
-    },
-
-    // // Get assets (images)
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     name: "images",
-    //     path: `${__dirname}/content/assets`,
-    //   },
-    // },
-
-    // Layout wrapper
-    {
-      resolve: "gatsby-plugin-layout",
-      options: {
-        component: require.resolve(`${__dirname}/src/index.js`),
-      },
-    },
-
-    // Fonts
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: ["Source Sans Pro", "Inconsolata", "IBM Plex Mono"],
-        },
-      },
-    },
-
-    // Google Analytics
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-116347511-1",
-        respectDNT: true,
-      },
-    },
-
-    // Manifest
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "Alex Luong",
-        short_name: "@alexluong",
-        start_url: ".",
-        theme_color: "#c800ec",
-        background_color: "#c800ec",
-        display: "minimal-ui",
-        icons: [
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        name: "images",
+        path: `${__dirname}/content/assets`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,7 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { graphql } from "gatsby"
-import PostList from "components/PostList"
 
-function IndexPage({
-  data: {
-    allContentfulBlogPost: { posts },
-  },
-}) {
-  return (
-    <div style={{ background: "#f4f8fb" }}>
-      <PostList posts={posts} />
-    </div>
-  )
-}
-
-IndexPage.propTypes = {
-  data: PropTypes.object.isRequired,
+function IndexPage() {
+  return <div>Hello World</div>
 }
 
 export default IndexPage
-
-export const query = graphql`
-  query IndexQuery {
-    allContentfulBlogPost(limit: 10, sort: { fields: [date], order: DESC }) {
-      posts: edges {
-        post: node {
-          id
-          title
-          slug
-          date(formatString: "MMMM DD, YYYY")
-          excerpt {
-            excerpt
-          }
-          categories {
-            name
-            slug
-          }
-          img: featuredImage {
-            description
-            fluid {
-              ...GatsbyContentfulFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
